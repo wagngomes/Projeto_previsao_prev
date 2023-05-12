6bea43924c278dc7fa77a048f244a86f270f7ddf
wagngomes gomes <wagner.gomes.cx2@gmail.com>
2023-05-12 00:58:40 +0000

    criando estrutura do sequelize

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 app.listen(3000, () => {
     console.log('servidor rodando')
 })
+
+app.use(bodyParser.json)
 
 app.get('/', (req, res) => {
     res.send('PROGRAMA RODANDO')
